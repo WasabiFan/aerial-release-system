7e79fa793155bd86947ac4d143b4907ad7ea8260
WasabiFan <wasabifan@outlook.com>
2016-05-03 10:06:09 -0700

    Refactor into 3 files

---
+++ RadioFuncs.ino
@@ -0,0 +1,124 @@
+
+void initializeRadio() {
+  printf_begin();
+
+  radio.begin();
+
+  radio.enableAckPayload();
+  radio.enableDynamicPayloads();
+
+  // open communication lines based on role
+#if ROLE == ROLE_GROUND
+  radio.openWritingPipe(pipeNames[0]);
+  radio.openReadingPipe(primaryPipeIndex, pipeNames[1]);
+#elif ROLE == ROLE_SKY
+  radio.openWritingPipe(pipeNames[1]);
+  radio.openReadingPipe(primaryPipeIndex, pipeNames[0]);
+#endif
+
+  radio.startListening();
+  radio.printDetails();
+  radio.writeAckPayload(primaryPipeIndex, &ackVal, 1);
+}
+
+#if ROLE == ROLE_SKY
+void handleAck() {
+  Serial.println(F("Heartbeat byte received"));
+
+  // Get the altitude value
+  float altitude = getAltitude();
+
+  // Construct the packet. Has a one-byte header.
+  byte packetData[sizeof(float) + 1] = { heightInitialVal };
+  // Get a pointer to the last 4 bytes of the packet buffer
+  // and set it to the altitude value
+  float* packetFloatPortion = (float*)&packetData[1];
+  *packetFloatPortion = altitude;
+
+  validateAck(sendData(&packetData, sizeof(packetData)));
+}
+#endif
+
+void validateAck(byte ackResponse) {
+  boolean isCorrectResponse = (ackResponse == ackVal);
+
+  if (isCorrectResponse) {
+    Serial.println(F("Comms healthy"));
+  } else {
+    Serial.print(F("Bad ack received: "));
+    Serial.println(ackResponse);
+    Serial.println(F("This probably means that the connection is unstable."));
+  }
+
+#if ROLE == ROLE_GROUND
+  isCommHealthy = isCorrectResponse;
+  // We have validated that data was successfully sent; we can consider this a heartbeat.
+  lastHeartbeat = millis();
+#endif
+}
+
+// receives numBytes bytes from the radio into recvBuf
+bool receiveData(void* recvBuf, uint8_t numBytes) {
+  if (radio.available()) {
+    radio.read(recvBuf, numBytes);
+    // Prep an ack for the next time we receive data
+    radio.writeAckPayload(primaryPipeIndex, &ackVal, 1);
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// receives a single byte from the radio
+byte receiveByte() {
+  byte val = 0;
+  receiveData(&val, 1);
+  return val;
+}
+
+// Sends arbitrary data of length numBytes from the given address
+byte sendData(void* val, int numBytes) {
+  byte recvByte;
+
+  if (radio.available()) {
+    Serial.println(F("Data available before request was sent! This probably means something was not read correctly."));
+  }
+  // Print bytes that are going to be sent
+  Serial.print(F("Sending data {"));
+  for (int i = 0; i < numBytes; i++)
+  {
+    Serial.print(((byte*)val)[i]);
+    if (i < numBytes - 1) {
+      Serial.print(F(", "));
+    }
+  }
+  Serial.println(F("}"));
+
+  // Stop listening temporarily so we can write
+  radio.stopListening();
+  bool writeSuccess = radio.write(val, numBytes);
+  radio.startListening();
+
+  if (writeSuccess) {
+    if (!radio.available()) {
+      Serial.println(F("Empty receive buffer! Was expecting an ack."));
+      return 0;
+    } else {
+      radio.read(&recvByte, 1);
+      radio.writeAckPayload(primaryPipeIndex, &ackVal, 1);
+
+      Serial.println(F("Received ack"));
+    }
+  } else {
+    Serial.println(F("Unknown radio write failure"));
+    return 0;
+  }
+
+  return recvByte;
+}
+
+// send a single byte on the radio
+byte sendByte(byte val)
+{
+  return sendData(&val, 1);
+}
--- WirelessTrigger.ino
+++ WirelessTrigger.ino
@@ -2,7 +2,11 @@
 #define ROLE_SKY 1
 
 // SET THE TARGET ROLE HERE -------------------------------
-#define ROLE ROLE_SKY
+#define ROLE ROLE_GROUND
+
+#if ROLE != ROLE_SKY && ROLE != ROLE_GROUND
+#error "ROLE must be ROLE_SKY or ROLE_GROUND"
+#endif
 
 // Standard libraries
 #include <Arduino.h>
@@ -18,7 +22,7 @@
 // Smart switch utils
 #include "~Switch.h"
 
-#else
+#elif ROLE == ROLE_SKY
 
 // Barometric pressure sensor
 #include <Wire.h>
@@ -27,7 +31,6 @@
 
 // Servo controller
 #include <Servo.h>
-
 #endif
 
 RF24 radio(7, 8);
@@ -43,11 +46,12 @@ const byte heightInitialVal = 0b01010101; // 85 decimal
 const byte ackVal = 0b11110000; // 240 decimal
 
 #if ROLE == ROLE_GROUND
+// time in between "heartbeat" signals - determine connection health and recieve back altitude
 #define HEARTBEAT_INTERVAL_MILLIS 300
-// Altitude is in feet
+// The threshold when the indicator LED changes
 #define ALTITUDE_TARGET_THRESH_FEET 100
 
-// can be digital pinsa
+// can be digital pins
 uint8_t triggerButtonPin = A0;
 uint8_t resetButtonPin = A1;
 uint8_t calibButtonPin = A2;
@@ -57,23 +61,23 @@ Switch resetButton = Switch(resetButtonPin);
 Switch calibButton = Switch(calibButtonPin);
 
 // can be digital pins
-uint8_t heartbeatLEDGreenPin = 5;
-uint8_t heartbeatLEDBluePin = 3;
-uint8_t heartbeatLEDRedPin = 6;
+uint8_t indicatorLEDGreenPin = 5;
+uint8_t indicatorLEDBluePin = 3;
+uint8_t indicatorLEDRedPin = 6;
 
 unsigned long lastHeartbeat = 0;
 bool isCommHealthy = false;
 
 float rawCalibrationAltitude = 0;
-bool isAltCalibrated = false;
-float lastAltitude = -1;
-float rawLastAltitude = -1;
+bool isAltitudeCalibrated = false;
+float lastAltitude = NAN; // NAN indicates that altitude is not stored yet
+float rawLastAltitude = NAN;
 
-#else
+#elif ROLE == ROLE_SKY
+
 #define TRIGGER_SERVO_ANGLE 90
 #define RESET_SERVO_ANGLE 0
 #define METERS_TO_FEET_RATIO (1250./381.)
-
 #define ALTITUDE_SMOOTHING 10
 
 // Should be a PWM pin
@@ -87,312 +91,144 @@ int lastAltitudeIndex;
 // Parameter is sensor ID
 Adafruit_BMP085_Unified bmp = Adafruit_BMP085_Unified(10085);
 bool bmpConnected = false;
+// TODO: power this from an actual power pin
 uint8_t bmpPowerPin = 4;
 #endif
 
 void setup()
 {
-    Serial.begin(115200);
-    printf_begin();
-
-    radio.begin();
-
-    radio.enableAckPayload();
-    radio.enableDynamicPayloads();
+  Serial.begin(115200);
+  initializeRadio();
 
 #if ROLE == ROLE_GROUND
-    radio.openWritingPipe(pipeNames[0]);
-    radio.openReadingPipe(primaryPipeIndex, pipeNames[1]);
-#else
-    radio.openWritingPipe(pipeNames[1]);
-    radio.openReadingPipe(primaryPipeIndex, pipeNames[0]);
-#endif
+  pinMode(indicatorLEDGreenPin, OUTPUT);
+  pinMode(indicatorLEDBluePin, OUTPUT);
+  pinMode(indicatorLEDRedPin, OUTPUT);
+#elif ROLE == ROLE_SKY
+  actuationServo.attach(servoPin);
+  actuationServo.write(RESET_SERVO_ANGLE);
 
-    radio.startListening();
-    radio.printDetails();
-    radio.writeAckPayload(primaryPipeIndex, &ackVal, 1);
+  pinMode(resetPin, OUTPUT);
+  digitalWrite(resetPin, HIGH);
 
-#if ROLE == ROLE_GROUND
-    pinMode(heartbeatLEDGreenPin, OUTPUT);
-    pinMode(heartbeatLEDBluePin, OUTPUT);
-    pinMode(heartbeatLEDRedPin, OUTPUT);
-#else
-    actuationServo.attach(servoPin);
-    actuationServo.write(RESET_SERVO_ANGLE);
+  // we don't have enough power pins, so we just power it from an output pin
+  pinMode(bmpPowerPin, OUTPUT);
+  digitalWrite(bmpPowerPin, HIGH);
 
-    pinMode(resetPin, OUTPUT);
-    digitalWrite(resetPin, HIGH);
-
-    pinMode(bmpPowerPin, OUTPUT);
-    digitalWrite(bmpPowerPin, HIGH);
-    Serial.print(F("BMP085 connecting"));
-    if (bmp.begin())
-    {
-        bmpConnected = true;
-        Serial.println(F("BMP085 connection succeeded"));
-    }
-    else
-        Serial.println(F("BMP085 connection failed"));
-
+  // initialize altimeter
+  Serial.print(F("BMP085 connecting... "));
+  if (bmp.begin()) {
+    bmpConnected = true;
+    Serial.println(F("Succeeded"));
+  } else {
+    Serial.println(F("Failed"));
+  }
 #endif
 }
 
+#if ROLE == ROLE_GROUND
+void loop() {
+  triggerButton.poll();
+  resetButton.poll();
+  calibButton.poll();
 
+  // Check for and handle incoming bytes first to make sure
+  // that buffers are clear. If they aren't, acks may not work correctly.
+  if (radio.available()) {
+    Serial.println(F("Data available"));
+    byte recvByte = receiveByte();
+
+    if (recvByte == heightInitialVal) {
+      // TODO: Figure out why the first byte isn't already gone
+
+      // Declare a buffer to store the packet and read the data into it
+      byte buffer[sizeof(float) + 1];
+      receiveData(&buffer, sizeof(buffer));
+
+      // Dereference the data portion of the packet as a float
+      rawLastAltitude = *(float*)(&(buffer[1]));
+      if (isAltitudeCalibrated) {
+        lastAltitude = rawLastAltitude - rawCalibrationAltitude;
+      } else {
+        lastAltitude = rawLastAltitude;
+      }
+      Serial.println("Got altitude " + String(lastAltitude));
+    } else {
+      Serial.println(F("Received unknown header data!"));
+    }
+  }
+
+  // Require a long press to activate
+  if (triggerButton.longPress()) {
+    Serial.println(F("Sending trigger byte"));
+    validateAck(sendByte(triggerVal));
+  }
+  else if (resetButton.longPress()) {
+    Serial.println(F("Sending reset byte"));
+    validateAck(sendByte(resetVal));
+  }
+  else if (calibButton.pushed()) {
+    if (isnan(rawLastAltitude)) {
+      // if we haven't received an altitude yet, we can't calibrate
+      Serial.println(F("Tried to calibrate, but haven't received altitude packet yet."));
+    } else {
+      rawCalibrationAltitude = rawLastAltitude;
+      Serial.println("New calibrated base altitude: " + String(rawCalibrationAltitude));
+      isAltitudeCalibrated = true;
+    }
+  }
+  else if (millis() - lastHeartbeat >= HEARTBEAT_INTERVAL_MILLIS) {
+    // Re-use ack byte for heartbeat
+    Serial.println(F("Sending ping"));
+    validateAck(sendByte(ackVal));
+  }
+
+  //Serial.println("Current calibrated altitude: " + String(lastAltitude));
+
+  bool altitudeAboveThreshold = isAltitudeCalibrated && !isnan(lastAltitude) && lastAltitude >= ALTITUDE_TARGET_THRESH_FEET;
+  if (isCommHealthy && altitudeAboveThreshold) {
+    setStatusLED(0, 255, 0); // Green
+  }
+  else if (isCommHealthy) {
+    setStatusLED(0, 0, 255); // Blue
+  }
+  else {
+    setStatusLED(255, 0, 0); // Red
+  }
+}
+
+#elif ROLE == ROLE_SKY
+void loop() {
+  if (radio.available()) {
+    byte recvByte = receiveByte();
+    switch (recvByte) {
+      case triggerVal:
+        Serial.println(F("Trigger byte received"));
+        actuationServo.write(TRIGGER_SERVO_ANGLE);
+        break;
+      case resetVal:
+        Serial.println(F("Reset byte received"));
+        actuationServo.write(RESET_SERVO_ANGLE);
+        break;
+      case ackVal:
+        handleAck();
+        break;
+      default:
+        Serial.print(F("Received unknown signal: "));
+        Serial.println((int)recvByte);
+        break;
+    }
+    checkAltimeterDataIntegrity();
+  }
+}
+#endif
 
 #if ROLE == ROLE_GROUND
 void setStatusLED(int r, int g, int b) {
-    analogWrite(heartbeatLEDRedPin, r);
-    analogWrite(heartbeatLEDGreenPin, g);
-    analogWrite(heartbeatLEDBluePin, b);
+  analogWrite(indicatorLEDRedPin, r);
+  analogWrite(indicatorLEDGreenPin, g);
+  analogWrite(indicatorLEDBluePin, b);
 }
-#else
-float getAltitude() {
-    if (bmpConnected) {
-        sensors_event_t event;
-        bmp.getEvent(&event);
-
-        float temperature;
-        bmp.getTemperature(&temperature);
-
-        float seaLevelPressure = SENSORS_PRESSURE_SEALEVELHPA;
-
-        float currentAlt = bmp.pressureToAltitude(seaLevelPressure, event.pressure, temperature) * METERS_TO_FEET_RATIO;
-        return smoothAltitude(currentAlt);
-    }
-
-    return -1;
-}
-float smoothAltitude(float newAlt) {
-    // cycle every time this is called
-    lastAltitudes[lastAltitudeIndex] = newAlt;
-    lastAltitudeIndex++;
-    if (lastAltitudeIndex >= ALTITUDE_SMOOTHING) {
-        lastAltitudeIndex = 0;
-    }
-
-    // get average
-    float sum = 0;
-    for (int i = 0; i < ALTITUDE_SMOOTHING; i++) {
-        sum += lastAltitudes[i];
-    }
-    return sum / ALTITUDE_SMOOTHING;
-}
-// this is to check and see if our barometer is just returning 0s and reset the arduino
-void checkBarometerDataIntegrity() {
-    // only check when our altitude is at the end in order to avoid the initial {0,0,0...}
-    if (lastAltitudeIndex != ALTITUDE_SMOOTHING) {
-        return;
-    }
-    bool allZeros = true;
-    for (int i = 0; i < ALTITUDE_SMOOTHING; i++) {
-        if (lastAltitudes[i] != 0.0f) {
-            allZeros = false;
-        }
-    }
-    if (allZeros) {
-        Serial.println("RESETTING ARDUINO");
-        digitalWrite(resetPin, LOW);
-    }
-
-}
-
 #endif
 
-void loop()
-{
-#if ROLE == ROLE_GROUND
 
-    triggerButton.poll();
-    resetButton.poll();
-    calibButton.poll();
-
-    // Check for and handle incoming bytes first to make sure
-    // that buffers are clear. If they aren't, acks may not work correctly.
-    if (radio.available()) {
-        Serial.println(F("Data available"));
-        byte recvByte = receiveByte();
-
-        if (recvByte == heightInitialVal)
-        {
-            // TODO: Figure out why the first byte isn't already gone
-
-            // Declare a buffer to store the packet and read the data into it
-            byte buffer[sizeof(float) + 1];
-            receiveData(&buffer, sizeof(buffer));
-
-            // Dereference the data portion of the packet as a float
-            rawLastAltitude = *(float*)(&(buffer[1]));
-            if (isAltCalibrated)
-                lastAltitude = rawLastAltitude - rawCalibrationAltitude;
-            else
-                lastAltitude = rawLastAltitude;
-
-            Serial.println("Got altitude " + String(lastAltitude));
-        }
-        else
-        {
-            Serial.println(F("Received unknown header data!"));
-        }
-    }
-
-    // Require a long press to activate
-    if (triggerButton.longPress()) {
-        Serial.println(F("Sending trigger byte"));
-
-        validateAck(sendByte(triggerVal));
-    }
-    else if (resetButton.longPress()) {
-        Serial.println(F("Sending reset byte"));
-
-        validateAck(sendByte(resetVal));
-    }
-    else if (calibButton.pushed()) {
-        // TODO: If we don't have a last altitude, complain
-        rawCalibrationAltitude = rawLastAltitude;
-        Serial.println("New calibrated base altitude: " + String(rawCalibrationAltitude));
-        isAltCalibrated = true;
-    }
-    else if (millis() - lastHeartbeat >= HEARTBEAT_INTERVAL_MILLIS) {
-        // Re-use ack byte for heartbeat
-        Serial.println(F("Sending ping"));
-        validateAck(sendByte(ackVal));
-    }
-
-    //Serial.println("Current calibrated altitude: " + String(lastAltitude));
-
-    bool altitudeAboveThreshold = isAltCalibrated && lastAltitude >= ALTITUDE_TARGET_THRESH_FEET;
-    if (isCommHealthy && altitudeAboveThreshold)
-        setStatusLED(0, 255, 0); // Green
-
-    else if (isCommHealthy)
-        setStatusLED(0, 0, 255); // Blue
-
-    else
-        setStatusLED(255, 0, 0); // Red
-#else
-    if (radio.available()) {
-        byte recvByte = receiveByte();
-        switch (recvByte) {
-        case triggerVal:
-            Serial.println(F("Trigger byte received"));
-            actuationServo.write(TRIGGER_SERVO_ANGLE);
-            break;
-        case resetVal:
-            Serial.println(F("Reset byte received"));
-            actuationServo.write(RESET_SERVO_ANGLE);
-            break;
-        case ackVal:
-        { // Enclose this in braces to localize variables
-            Serial.println(F("Heartbeat byte received"));
-
-            // Get the altitude value
-            float altitude = getAltitude();
-
-            // Construct the packet. Has a one-byte header.
-            byte packetData[sizeof(float) + 1] = { heightInitialVal };
-            // Get a pointer to the last 4 bytes of the packet buffer 
-            // and set it to the altitude value
-            float* packetFloatPortion = (float*)&packetData[1];
-            *packetFloatPortion = altitude;
-
-            validateAck(sendData(&packetData, sizeof(packetData)));
-            break;
-        }
-        default:
-            Serial.print(F("Received unknown signal "));
-            Serial.println((int)recvByte);
-            break;
-        }
-        checkBarometerDataIntegrity();
-
-    }
-#endif
-}
-
-
-void validateAck(byte ackResponse) {
-    boolean correctResponse = (ackResponse == ackVal);
-
-    if (correctResponse) {
-        Serial.println(F("Comms healthy"));
-    }
-    else {
-        Serial.print(F("Bad ack received: "));
-        Serial.println(ackResponse);
-        Serial.println(F("This probably means that the connection is unstable."));
-    }
-
-#if ROLE == ROLE_GROUND
-    isCommHealthy = correctResponse;
-    // We have validated that data was successfully sent; we can consider this a heartbeat.
-    lastHeartbeat = millis();
-#endif
-}
-
-// receives numBytes bytes from the radio into recvBuf
-bool receiveData(void* recvBuf, uint8_t numBytes) {
-    if (radio.available()) {
-        radio.read(recvBuf, numBytes);
-        // Prep an ack for the next time we receive data
-        radio.writeAckPayload(primaryPipeIndex, &ackVal, 1);
-        return true;
-    }
-    else {
-        return false;
-    }
-}
-
-// receives a single byte from the radio
-byte receiveByte() {
-    byte val = 0;
-    receiveData(&val, 1);
-    return val;
-}
-
-// Sends arbitrary data of length numBytes from the given address
-byte sendData(void* val, int numBytes) {
-    byte recvByte;
-
-    if (radio.available())
-        Serial.println(F("Data available before request was sent! This probably means something was not read correctly."));
-
-    // Print bytes that are going to be sent
-    Serial.print(F("Sending data {"));
-    for (int i = 0; i < numBytes; i++)
-    {
-        Serial.print(((byte*)val)[i]);
-        if (i < numBytes - 1)
-            Serial.print(F(", "));
-    }
-    Serial.println(F("}"));
-
-    // Stop listening temporarily so we can write
-    radio.stopListening();
-    bool writeSuccess = radio.write(val, numBytes);
-    radio.startListening();
-
-    if (writeSuccess) {
-        if (!radio.available()) {
-            Serial.println(F("Empty receive buffer! Was expecting an ack."));
-            return 0;
-        }
-        else {
-            radio.read(&recvByte, 1);
-            radio.writeAckPayload(primaryPipeIndex, &ackVal, 1);
-
-            Serial.println(F("Received ack"));
-        }
-    }
-    else {
-        Serial.println(F("Unknown radio write failure"));
-        return 0;
-    }
-
-    return recvByte;
-}
-
-// send a single byte on the radio
-byte sendByte(byte val)
-{
-    return sendData(&val, 1);
-}
