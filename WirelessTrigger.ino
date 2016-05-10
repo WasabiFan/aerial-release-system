52882baa0bf8afdd55b980712735770b4b77a95a
WasabiFan <wasabifan@outlook.com>
2016-05-10 13:11:36 -0700

    Format main code files

--- WirelessTrigger.ino
+++ WirelessTrigger.ino
@@ -82,145 +82,148 @@ uint8_t bmpPowerPin = 4;
 
 void setup()
 {
-  Serial.begin(115200);
-  initializeRadio();
+    Serial.begin(115200);
+    initializeRadio();
 
 #if ROLE == ROLE_GROUND
-  pinMode(indicatorLEDGreenPin, OUTPUT);
-  pinMode(indicatorLEDBluePin, OUTPUT);
-  pinMode(indicatorLEDRedPin, OUTPUT);
+    pinMode(indicatorLEDGreenPin, OUTPUT);
+    pinMode(indicatorLEDBluePin, OUTPUT);
+    pinMode(indicatorLEDRedPin, OUTPUT);
 #elif ROLE == ROLE_SKY
-  actuationServo.attach(servoPin);
-  actuationServo.write(RESET_SERVO_ANGLE);
+    actuationServo.attach(servoPin);
+    actuationServo.write(RESET_SERVO_ANGLE);
 
-  pinMode(resetPin, OUTPUT);
-  digitalWrite(resetPin, HIGH);
+    pinMode(resetPin, OUTPUT);
+    digitalWrite(resetPin, HIGH);
 
-  // we don't have enough power pins, so we just power it from an output pin
-  pinMode(bmpPowerPin, OUTPUT);
-  digitalWrite(bmpPowerPin, HIGH);
+    // we don't have enough power pins, so we just power it from an output pin
+    pinMode(bmpPowerPin, OUTPUT);
+    digitalWrite(bmpPowerPin, HIGH);
 
-  // initialize altimeter
-  Serial.print(F("BMP085 connecting... "));
-  if (bmp.begin()) {
-    bmpConnected = true;
-    Serial.println(F("Succeeded"));
-  } else {
-    Serial.println(F("Failed"));
-  }
+    // initialize altimeter
+    Serial.print(F("BMP085 connecting... "));
+    if (bmp.begin()) {
+        bmpConnected = true;
+        Serial.println(F("Succeeded"));
+    }
+    else {
+        Serial.println(F("Failed"));
+    }
 #endif
 }
 
 #if ROLE == ROLE_GROUND
 void loop() {
-  triggerButton.poll();
-  resetButton.poll();
-  calibButton.poll();
+    triggerButton.poll();
+    resetButton.poll();
+    calibButton.poll();
 
-  // Check for and handle incoming bytes first to make sure
-  // that buffers are clear. If they aren't, acks may not work correctly.
-  if (radio.available()) {
-    Serial.println(F("Data available"));
-    byte recvByte = receiveByte();
+    // Check for and handle incoming bytes first to make sure
+    // that buffers are clear. If they aren't, acks may not work correctly.
+    if (radio.available()) {
+        Serial.println(F("Data available"));
+        byte recvByte = receiveByte();
 
-    if (recvByte == heightInitialVal) {
-      // TODO: Figure out why the first byte isn't already gone
+        if (recvByte == heightInitialVal) {
+            // TODO: Figure out why the first byte isn't already gone
 
-      // Declare a buffer to store the packet and read the data into it
-      byte buffer[sizeof(float) + 1];
-      receiveData(&buffer, sizeof(buffer));
+            // Declare a buffer to store the packet and read the data into it
+            byte buffer[sizeof(float) + 1];
+            receiveData(&buffer, sizeof(buffer));
 
-      // Dereference the data portion of the packet as a float
-      rawLastAltitude = *(float*)(&(buffer[1]));
-      if (isAltitudeCalibrated) {
-        lastAltitude = rawLastAltitude - rawCalibrationAltitude;
-      } else {
-        lastAltitude = rawLastAltitude;
-      }
-      Serial.println("Got altitude " + String(lastAltitude));
-    } else {
-      Serial.println(F("Received unknown header data!"));
+            // Dereference the data portion of the packet as a float
+            rawLastAltitude = *(float*)(&(buffer[1]));
+            if (isAltitudeCalibrated) {
+                lastAltitude = rawLastAltitude - rawCalibrationAltitude;
+            }
+            else {
+                lastAltitude = rawLastAltitude;
+            }
+            Serial.println("Got altitude " + String(lastAltitude));
+        }
+        else {
+            Serial.println(F("Received unknown header data!"));
+        }
     }
-  }
 
-  // Require a long press to activate
-  if (triggerButton.longPress()) {
-    Serial.println(F("Sending trigger byte"));
-    if (validateAck(sendByte(triggerVal)))
-        lastCommandTime = millis();
-  }
-  else if (resetButton.longPress()) {
-    Serial.println(F("Sending reset byte"));
-    if(validateAck(sendByte(resetVal)))
-        lastCommandTime = millis();
-  }
-  else if (calibButton.pushed()) {
-    if (isnan(rawLastAltitude)) {
-      // if we haven't received an altitude yet, we can't calibrate
-      Serial.println(F("Tried to calibrate, but haven't received altitude packet yet."));
+    // Require a long press to activate
+    if (triggerButton.longPress()) {
+        Serial.println(F("Sending trigger byte"));
+        if (validateAck(sendByte(triggerVal)))
+            lastCommandTime = millis();
+    }
+    else if (resetButton.longPress()) {
+        Serial.println(F("Sending reset byte"));
+        if (validateAck(sendByte(resetVal)))
+            lastCommandTime = millis();
+    }
+    else if (calibButton.pushed()) {
+        if (isnan(rawLastAltitude)) {
+            // if we haven't received an altitude yet, we can't calibrate
+            Serial.println(F("Tried to calibrate, but haven't received altitude packet yet."));
+        }
+        else {
+            rawCalibrationAltitude = rawLastAltitude;
+            Serial.println("New calibrated base altitude: " + String(rawCalibrationAltitude));
+            isAltitudeCalibrated = true;
+
+            lastCommandTime = millis();
+        }
+    }
+    else if (millis() - lastHeartbeat >= HEARTBEAT_INTERVAL_MILLIS) {
+        // Re-use ack byte for heartbeat
+        Serial.println(F("Sending ping"));
+        validateAck(sendByte(ackVal));
+    }
+
+    //Serial.println("Current calibrated altitude: " + String(lastAltitude));
+
+    bool altitudeAboveThreshold = isAltitudeCalibrated && !isnan(lastAltitude) && lastAltitude >= ALTITUDE_TARGET_THRESH_FEET;
+    if (millis() - lastCommandTime <= COMMAND_SIG_DURATION) {
+        setStatusLED(255, 255, 0); // Yellow
+    }
+    else if (isCommHealthy && altitudeAboveThreshold) {
+        setStatusLED(0, 255, 0); // Green
+    }
+    else if (isCommHealthy) {
+        setStatusLED(0, 0, 255); // Blue
     }
     else {
-      rawCalibrationAltitude = rawLastAltitude;
-      Serial.println("New calibrated base altitude: " + String(rawCalibrationAltitude));
-      isAltitudeCalibrated = true;
-
-      lastCommandTime = millis();
+        setStatusLED(255, 0, 0); // Red
     }
-  }
-  else if (millis() - lastHeartbeat >= HEARTBEAT_INTERVAL_MILLIS) {
-    // Re-use ack byte for heartbeat
-    Serial.println(F("Sending ping"));
-    validateAck(sendByte(ackVal));
-  }
-
-  //Serial.println("Current calibrated altitude: " + String(lastAltitude));
-
-  bool altitudeAboveThreshold = isAltitudeCalibrated && !isnan(lastAltitude) && lastAltitude >= ALTITUDE_TARGET_THRESH_FEET;
-  if (millis() - lastCommandTime <= COMMAND_SIG_DURATION) {
-    setStatusLED(255, 255, 0); // Yellow
-  }
-  else if (isCommHealthy && altitudeAboveThreshold) {
-    setStatusLED(0, 255, 0); // Green
-  }
-  else if (isCommHealthy) {
-    setStatusLED(0, 0, 255); // Blue
-  }
-  else {
-    setStatusLED(255, 0, 0); // Red
-  }
 }
 
 #elif ROLE == ROLE_SKY
 void loop() {
-  if (radio.available()) {
-    byte recvByte = receiveByte();
-    switch (recvByte) {
-      case triggerVal:
-        Serial.println(F("Trigger byte received"));
-        actuationServo.write(TRIGGER_SERVO_ANGLE);
-        break;
-      case resetVal:
-        Serial.println(F("Reset byte received"));
-        actuationServo.write(RESET_SERVO_ANGLE);
-        break;
-      case ackVal:
-        handleAck();
-        break;
-      default:
-        Serial.print(F("Received unknown signal: "));
-        Serial.println((int)recvByte);
-        break;
+    if (radio.available()) {
+        byte recvByte = receiveByte();
+        switch (recvByte) {
+        case triggerVal:
+            Serial.println(F("Trigger byte received"));
+            actuationServo.write(TRIGGER_SERVO_ANGLE);
+            break;
+        case resetVal:
+            Serial.println(F("Reset byte received"));
+            actuationServo.write(RESET_SERVO_ANGLE);
+            break;
+        case ackVal:
+            handleAck();
+            break;
+        default:
+            Serial.print(F("Received unknown signal: "));
+            Serial.println((int)recvByte);
+            break;
+        }
+        checkAltimeterDataIntegrity();
     }
-    checkAltimeterDataIntegrity();
-  }
 }
 #endif
 
 #if ROLE == ROLE_GROUND
 void setStatusLED(int r, int g, int b) {
-  analogWrite(indicatorLEDRedPin, r);
-  analogWrite(indicatorLEDGreenPin, g);
-  analogWrite(indicatorLEDBluePin, b);
+    analogWrite(indicatorLEDRedPin, r);
+    analogWrite(indicatorLEDGreenPin, g);
+    analogWrite(indicatorLEDBluePin, b);
 }
 #endif
 
