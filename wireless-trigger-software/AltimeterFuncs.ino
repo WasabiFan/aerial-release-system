2355d06d1be963c9ade8bd851dd2f6fb3ca41a8a
WasabiFan <wasabifan@outlook.com>
2017-06-08 10:18:10 -0700

    Migrate to proper GitHub repo

---
+++ wireless-trigger-software/AltimeterFuncs.ino
@@ -0,0 +1,54 @@
+#if ROLE == ROLE_SKY
+// returns smoothed uncalibrated altitude
+float getAltitude() {
+    if (!bmpConnected) {
+        return -1;
+    }
+
+    sensors_event_t event;
+    bmp.getEvent(&event);
+
+    float temperature;
+    bmp.getTemperature(&temperature);
+
+    float seaLevelPressure = SENSORS_PRESSURE_SEALEVELHPA;
+
+    float currentAlt = bmp.pressureToAltitude(seaLevelPressure, event.pressure, temperature) * METERS_TO_FEET_RATIO;
+    return smoothAltitude(currentAlt);
+}
+
+// outputs a running average of the last n (ALTITUDE_SMOOTHING) altitudes
+float smoothAltitude(float newAlt) {
+    // cycle every time this is called
+    lastAltitudes[lastAltitudeIndex] = newAlt;
+    lastAltitudeIndex++;
+    if (lastAltitudeIndex >= ALTITUDE_SMOOTHING) {
+        lastAltitudeIndex = 0;
+    }
+
+    // get average
+    float sum = 0;
+    for (int i = 0; i < ALTITUDE_SMOOTHING; i++) {
+        sum += lastAltitudes[i];
+    }
+    return sum / ALTITUDE_SMOOTHING;
+}
+// this is to check and see if our altimeter is just returning 0s and reset the arduino
+void checkAltimeterDataIntegrity() {
+    // only check when our altitude is at the end in order to avoid the initial {0,0,0...}
+    if (lastAltitudeIndex != ALTITUDE_SMOOTHING) {
+        return;
+    }
+    bool allZeros = true;
+    for (int i = 0; i < ALTITUDE_SMOOTHING; i++) {
+        if (lastAltitudes[i] != 0.0f) {
+            allZeros = false;
+        }
+    }
+    if (allZeros) {
+        Serial.println("RESETTING ARDUINO");
+        digitalWrite(resetPin, LOW);
+    }
+
+}
+#endif
---
+++ wireless-trigger-software/Config.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#define METERS_TO_FEET_RATIO (1250./381.)
+
+#define ROLE_GROUND 0
+#define ROLE_SKY 1
+
+// SET THE TARGET ROLE HERE -------------------------------
+#define ROLE ROLE_SKY
+
+#if ROLE != ROLE_SKY && ROLE != ROLE_GROUND
+#error "ROLE must be ROLE_SKY or ROLE_GROUND"
+#endif
+
+// time in between "heartbeat" signals - determine connection health and recieve back altitude
+#define HEARTBEAT_INTERVAL_MILLIS 300
+// The threshold when the indicator LED changes
+#define ALTITUDE_TARGET_THRESH_FEET 100
+
+// Angle when servo is in "trigger" position
+#define TRIGGER_SERVO_ANGLE 90
+// Angle when servo is in reset/ready position
+#define RESET_SERVO_ANGLE 0
+// Number of altitude samples to average
+#define ALTITUDE_SMOOTHING 10
+
+// Time (in ms) that the indicator LED will flash for when a command is successfully registered
+#define COMMAND_SIG_DURATION 100
